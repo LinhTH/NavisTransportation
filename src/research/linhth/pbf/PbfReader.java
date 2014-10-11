7efa5ee9a9a134639a80b0b300ace0279423e3fa
Trương Hoàng Linh <truonghoanglinhk55bkhn@gmail.com>
2014-10-11 10:21:32 +0700

    update

---
+++ src/research/linhth/pbf/PbfReader.java
@@ -0,0 +1,13 @@
+package research.linhth.pbf;
+
+import java.io.InputStream;
+
+/**
+ * Reading an OSM data source from a PBF file.
+ * @author rimberry
+ * @refer: graphhopper.com
+ *
+ */
+public class PbfReader {
+	private InputStream inputStream;
+}
