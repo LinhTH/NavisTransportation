724e4195ca84a0836a12bf7629ba72f9abe6bc01
Trương Hoàng Linh <truonghoanglinhk55bkhn@gmail.com>
2014-10-11 10:42:18 +0700

    add OSMElement

---
+++ src/research/linhth/OSM/OSMElement.java
@@ -0,0 +1,12 @@
+package research.linhth.OSM;
+
+/*
+ * @author: graphhopper.com
+ */
+
+public abstract class OSMElement {
+	public static final int NODE = 0;
+	public static final int WAY  = 1;
+	public static final int RELATION = 2;
+	
+}
